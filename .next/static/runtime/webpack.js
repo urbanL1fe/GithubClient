c00981ad6af3ffe0991c433aab117ecfd460cd0a
Nikos <avramidis1nikos@gmail.com>
2019-04-21 18:32:16 +0300

    Small changes for deployment

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "07243d0e9c0dfc8b7aec";
+/******/ 	var hotCurrentHash = "5eb11632a6c7c298278b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
