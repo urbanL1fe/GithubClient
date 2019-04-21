5004477e7c0799d8210f5cdd13075155d626dc24
Nikos <avramidis1nikos@gmail.com>
2019-04-21 19:14:35 +0300

    index rename

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import UserList from '../components/UserList';
 import PageLayout from '../components/PageLayout';
 import '../styles.scss';
 
+console.log('Welcome!');
+
 const Index = () => (
   <PageLayout>
     <UserList />
